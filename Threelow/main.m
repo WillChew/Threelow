e83b1593a0eee4e636bcae2c1ec3ffdefbaf9bc2
WillChew <williamchew95@gmail.com>
2018-06-06 16:21:09 -0400

    Initial Commit

---
+++ Threelow/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Threelow
+//
+//  Created by Will Chew on 2018-06-06.
+//  Copyright © 2018 Will Chew. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
